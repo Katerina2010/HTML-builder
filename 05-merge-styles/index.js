8b237c3ea13b0b9d71015ca80640de7403c950b2
Katerina Bolshakova <e.bolshakova2010@gmail.com>
2023-05-05 18:08:34 +0300

    feat: update function, solution not changed

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,25 +1,28 @@
 const fs = require('fs');
-const fsPromose = require('fs/promises');
+const fsPromise = require('fs/promises');
 const path = require('path');
 
-(async () => {
+async function bundleStyle(dir, bundle ) {
   try {
-    const folderStyle = 'styles';
-    const folderStylePath = path.join(__dirname, folderStyle);
-    const dst = path.join(__dirname, 'project-dist');
-    const fileBundle  = path.join(dst, 'bundle.css');
-    const streamBundle = fs.createWriteStream(fileBundle);
+    const streamBundle = fs.createWriteStream(bundle);
 
-    const srcFiles = await fsPromose.readdir(folderStylePath, {withFileTypes: true});
+    const srcFiles = await fsPromise.readdir(dir, {withFileTypes: true});
 
     for (const item of srcFiles) {
       if (item.isFile() && path.extname(item.name) === '.css') {
-        const currentContent = await fsPromose.readFile(path.join(folderStylePath, item.name));
-        streamBundle.write(currentContent);
+        const currentContent = await fsPromise.readFile(path.join(dir, item.name));
+        streamBundle.write(currentContent + '\n');
       }
     }
     console.log(" File bundle.css is ready");
   } catch (error) {
     console.error('Error:', error.message);
   }
-})();
+}
+ 
+  const folderStylePath = path.join(__dirname, 'styles');
+  const dst = path.join(__dirname, 'project-dist');
+  const fileBundle  = path.join(dst, 'bundle.css');
+
+  bundleStyle(folderStylePath, fileBundle );
+  
